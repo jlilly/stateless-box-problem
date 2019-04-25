a8a6b8df4e62eed0e0038bc000a9d57be0363d2a
Joe <joe.a.lilly@gmail.com>
2019-04-25 02:42:02 -0600

    Test cases for largetMatrix

--- src/app/largest-matrix.service.spec.ts
+++ src/app/largest-matrix.service.spec.ts
@@ -35,6 +35,60 @@ describe('LargestMatrixService', () => {
     });
   });
 
+  // TODO: this may become a large set of tests, move into own file?
+  [{
+    description: 'Gracefully handles bad input: undefined',
+    arr: undefined,
+    expectedResult: 0
+  }, {
+    description: 'Gracefully handles bad input: null',
+    arr: null,
+    expectedResult: 0
+  }, {
+    description: 'Gracefully handles bad input: []',
+    arr: [],
+    expectedResult: 0
+  }, {
+    description: 'Gracefully handles bad input: [[]]',
+    arr: [[]],
+    expectedResult: 0
+  }, {
+    description: 'Zero Case',
+    arr: [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]],
+    expectedResult: 0
+  }, {
+    description: 'One Case',
+    arr: [[0, 0, 0],
+          [0, 1, 0],
+          [0, 0, 0]],
+    expectedResult: 1
+  }, {
+    description: 'Two Case',
+    arr: [[1, 0, 0],
+          [0, 1, 0],
+          [0, 0, 0]],
+    expectedResult: 1
+  }, {
+    description: 'Three Case',
+    arr: [[1, 0, 0],
+          [0, 1, 0],
+          [0, 0, 1]],
+    expectedResult: 1
+  }, {
+    description: 'Four doesn\'t mean a square',
+    arr: [[1, 0, 0],
+          [0, 1, 0],
+          [1, 0, 1]],
+    expectedResult: 1
+  }].forEach( testCase => {
+    it(testCase.description, () => {
+      const result = service.largestMatrix(testCase.arr);
+
+      expect(result).toEqual(testCase.expectedResult);
+    });
+  });
 
 
 });
