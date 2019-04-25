672471b53fb9bac2bda9bbe0162823a76d41d6b1
Joe <joe.a.lilly@gmail.com>
2019-04-25 13:44:15 -0600

    ng g component visualizer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { VisualizerComponent } from './visualizer/visualizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualizerComponent
   ],
   imports: [
     BrowserModule,
