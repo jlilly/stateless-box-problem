b0fb5345098beffd6064d2e75f43865583793548
Joe <joe.a.lilly@gmail.com>
2019-04-25 13:05:13 -0600

    Comments / Documentation

--- src/app/largest-matrix.service.ts
+++ src/app/largest-matrix.service.ts
@@ -8,6 +8,12 @@ export class LargestMatrixService {
 
   constructor() { }
 
+  /**
+   * Finds the largest square of 1s in an array of numbers.
+   * Expanded upon original problem slightly by handling bad input.
+   * @param arr A 2d array of 0s and 1s
+   * @returns The side length for the largest square of 1s
+   */
   public largestMatrix(arr: number[][]): number {
     if ( isNullOrUndefined(arr) ||  arr.length === 0 ) {
       return 0;
@@ -16,15 +22,26 @@ export class LargestMatrixService {
       return 0;
     }
 
+    // TODO: could add guard to check for in bounds input
+    //       (eg is anything not 0 or 1?)
+
+    // TODO: could add check to see if rows are of uneven length
+    //       and trim to smallest
+
+    // Start our search reasonably
     const maxPossibleLength = Math.floor( Math.sqrt( this.sum(arr) ) );
 
     if ( maxPossibleLength < 2 ) {
+      // There are either NO squares or only groups of 1
       return maxPossibleLength;
     }
 
+    // Bad name, but don't want to recalc the chain data over and over...
     const chainsMaster = arr.map( this.analyseRow );
 
+    // Go from biggest to smallest possible square
     for ( let largest = maxPossibleLength; largest > 1; --largest ) {
+      // Filter out chains that are too small
       const chains = chainsMaster.map( row => {
         return row.filter(c => c.length >= largest);
       });
@@ -66,8 +83,11 @@ export class LargestMatrixService {
     return 1;
   }
 
-  /** Sum up all the 1s.
+  /**
+   * Sum up all the 1s.
    * Useful for finding maximum possible square.
+   * @param arr The input array for the whole problem
+   * @returns The sum of the matrix
    */
   private sum(arr: number[][]): number {
     return arr.map(
@@ -76,7 +96,12 @@ export class LargestMatrixService {
       }).reduce( (prev, curr) => prev + curr );
   }
 
-  // create list of strings of 1s in each row
+  /**
+   * Maps a raw input row into a list of chains of 1s.
+   * This meta data is useful so we don't have to brute force the square hunt
+   * @param row Row from the input array
+   * @returns An array containing the index and lengths of each chain
+   */
   private analyseRow(row: number[]): { index: number, length: number }[] {
     const chainData = [];
     let start = 0;
