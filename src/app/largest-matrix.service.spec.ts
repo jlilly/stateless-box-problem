a1390cbea413ee96b25770870e4b13cc231f16f6
Joe <joe.a.lilly@gmail.com>
2019-04-25 02:21:15 -0600

    scaffolding, sum() and basic testing

--- src/app/largest-matrix.service.spec.ts
+++ src/app/largest-matrix.service.spec.ts
@@ -1,12 +1,40 @@
-import { TestBed } from '@angular/core/testing';
-
 import { LargestMatrixService } from './largest-matrix.service';
 
 describe('LargestMatrixService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: LargestMatrixService;
 
-  it('should be created', () => {
-    const service: LargestMatrixService = TestBed.get(LargestMatrixService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    service = new LargestMatrixService();
   });
+
+  [{
+    description: 'Simplest case, 0',
+    arr: [[0]],
+    expectedResult: 0
+  }, {
+    description: 'One case, 1',
+    arr: [[1]],
+    expectedResult: 1
+  }, {
+    description: '1 + 1 + 1 + 1 + 1 = 5',
+    arr: [[1, 0, 1],
+          [0, 0, 0],
+          [1, 1, 1]],
+    expectedResult: 5
+  }, {
+    description: 'Can handle all numbers',
+    arr: [[1, 0, -1],
+          [0, 6, 0],
+          [.5, 1, 1]],
+    expectedResult: 8.5
+  }].forEach( testCase => {
+    it(testCase.description, () => {
+      const result = (service as any).sum(testCase.arr);
+
+      expect(result).toEqual(testCase.expectedResult);
+    });
+  });
+
+
+
 });
--- src/app/largest-matrix.service.ts
+++ src/app/largest-matrix.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class LargestMatrixService {
 
   constructor() { }
+
+  public largestMatrix(arr: number[][]): number {
+    const maxPossibleLength = this.sum(arr);
+    return 0;
+  }
+
+  /** Sum up all the 1s.
+   * Useful for finding maximum possible square.
+   */
+  private sum(arr: number[][]): number {
+    return arr.map(
+      row => {
+        return row.reduce( (prev, curr) => prev + curr );
+      }).reduce( (prev, curr) => prev + curr );
+  }
+
+  // TODO: type output
+  // create list of strings of 1s in each row
+  private analyseRow(arr: number[][]): any[] {
+    return [];
+  }
 }
